d8cb95cad805b7f6c5932d5980e5e0a1ce68d63a
walter <00189617@uca.edu.sv>
2020-08-26 17:51:22 -0600

    lol

---
+++ docs/precache-manifest.2ae7a0716a9e188814a86f357f00e890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0516c6addff876a9a2db32a84c1ac6b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edb11e3cb4e6d1323e86",
+    "url": "/static/css/main.376d4f73.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb11e3cb4e6d1323e86",
+    "url": "/static/js/main.331d2a4c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
